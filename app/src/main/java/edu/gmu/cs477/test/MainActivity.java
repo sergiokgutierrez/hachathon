9d794f6a9f21c4c75db5970d6ac2d75577c6ebd1
Sergio <Gutierrez>
2017-12-02 19:07:24 -0500

    first commit

---
+++ app/src/main/java/edu/gmu/cs477/test/MainActivity.java
@@ -0,0 +1,39 @@
+package edu.gmu.cs477.test;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.Menu;
+
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+    ListView lv;
+    Context context;
+
+    ArrayList prgmName;
+    public static int [] prgmImages={R.drawable.images,R.drawable.images1,R.drawable.images2,R.drawable.images3,R.drawable.images4,R.drawable.images5,R.drawable.images6,R.drawable.images7,R.drawable.images8};
+    public static String [] prgmNameList={"Bitcoin\t\t25","Etherium","Ripple","Litecoin","Cardone ","NEO","EOS","Qtum","Zcash"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context=this;
+
+        lv=(ListView) findViewById(R.id.listView);
+        lv.setAdapter(new CustomAdapter(this, prgmNameList,prgmImages));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+}
